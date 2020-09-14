3cb4cfa9f29cc1ee243c0a0cbd773447fc4c2ab3
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2020-09-15 00:23:29 +0300

    Report

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int opening_balance = 2_000_000_000;
+        int transfer = 500_000_000 ;
+        int final_balance = opening_balance + transfer;
+        System.out.println(final_balance);
+    }
+}
